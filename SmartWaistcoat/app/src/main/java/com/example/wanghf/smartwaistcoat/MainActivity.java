baf6dca5cefe910bd9ce3fb37d2c7efca3316738
wanghf <wanghf1010@gmail.com>
2017-04-14 21:32:18 +0800

    add XYplot

--- SmartWaistcoat/app/src/main/java/com/example/wanghf/smartwaistcoat/MainActivity.java
+++ SmartWaistcoat/app/src/main/java/com/example/wanghf/smartwaistcoat/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.wanghf.smartwaistcoat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
+import com.androidplot.xy.XYPlot;
 import com.example.wanghf.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private XYPlot impulsePlot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    private void initPlots() {
+        impulsePlot = (XYPlot) findViewById(R.id.main_plot_impulse);
+    }
+
 }
