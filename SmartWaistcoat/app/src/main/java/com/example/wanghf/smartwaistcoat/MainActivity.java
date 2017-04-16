6ffb44ce200d813b6bebfad991a60fd10950e1f0
wanghf <wanghf1010@gmail.com>
2017-04-16 23:40:47 +0800

    switch display

--- SmartWaistcoat/app/src/main/java/com/example/wanghf/smartwaistcoat/MainActivity.java
+++ SmartWaistcoat/app/src/main/java/com/example/wanghf/smartwaistcoat/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.wanghf.smartwaistcoat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 import com.androidplot.xy.XYPlot;
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private XYPlot impulsePlot;
+
+    private boolean showingCurve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,4 +27,16 @@ public class MainActivity extends AppCompatActivity {
         impulsePlot = (XYPlot) findViewById(R.id.main_plot_impulse);
     }
 
+    public void onClickShowCurve(View view) {
+        if (showingCurve) {
+            findViewById(R.id.linear_curve).setVisibility(View.GONE);
+            findViewById(R.id.linear_table).setVisibility(View.VISIBLE);
+            showingCurve = false;
+        }
+        else {
+            findViewById(R.id.linear_table).setVisibility(View.GONE);
+            findViewById(R.id.linear_curve).setVisibility(View.VISIBLE);
+            showingCurve = true;
+        }
+    }
 }
