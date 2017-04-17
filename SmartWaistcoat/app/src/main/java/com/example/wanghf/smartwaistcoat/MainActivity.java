e25c2d8b2a758fa4efd2224fa1090fdb9c27173a
wanghf <18810690716@126.com>
2017-04-17 18:19:08 +0800

    merge

--- SmartWaistcoat/app/src/main/java/com/example/wanghf/smartwaistcoat/MainActivity.java
+++ SmartWaistcoat/app/src/main/java/com/example/wanghf/smartwaistcoat/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.wanghf.smartwaistcoat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.wanghf.myapplication.R;
 
@@ -11,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickShowAcceleration(View view) {
+
     }
 }
