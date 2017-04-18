75444b74c4a1085edf9d9777ee3d691f92c466e9
wanghf <18810690716@126.com>
2017-04-18 21:01:10 +0800

    add netty dependency

---
+++ SmartWaistcoat/app/src/main/java/com/example/wanghf/smartwaistcoat/inputdata/OnDataRecvedListener.java
@@ -0,0 +1,8 @@
+package com.example.wanghf.smartwaistcoat.inputdata;
+
+/**
+ * Created by SmallLong on 2016/7/7.
+ */
+public interface OnDataRecvedListener {
+    public void newDataCome(String data);
+}
