763849dd7856f0fdcb0b4868b00557af9dcbe09e
wanghf <18810690716@126.com>
2017-05-07 22:01:59 +0800

    更新数据解析协议

--- SmartWaistcoat/app/src/main/java/com/example/wanghf/smartwaistcoat/inputdata/DataParseService.java
+++ SmartWaistcoat/app/src/main/java/com/example/wanghf/smartwaistcoat/inputdata/DataParseService.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.example.wanghf.smartwaistcoat.MainApplication;
@@ -36,7 +35,7 @@ public class DataParseService extends Service {
     private String dir = "AAA";
     private String fileName = "gnss.txt";
     private LinkedBlockingQueue<WaistcoatData> queue;
-    private LinkedBlockingQueue<Byte> bytes;
+    private LinkedBlockingQueue<Byte> bytesQueue;
     private Context context;
 
     public void onCreate() {
@@ -46,8 +45,8 @@ public class DataParseService extends Service {
         if(queue == null){
             queue = MainApplication.getQueue();
         }
-        if (bytes == null) {
-            bytes = MainApplication.getBytes();
+        if (bytesQueue == null) {
+            bytesQueue = MainApplication.getBytes();
         }
         if (translateThread == null) {
             translateThread = new TranslateThread();
@@ -60,77 +59,55 @@ public class DataParseService extends Service {
         super.onCreate();
     }
     private class TranslateThread extends Thread {
-        private int count = 0;
         private final int noneHead = 0;
-        private final int gpsState = 1;
-        private final int navState = 2;
-        private int gpsCount = 0;
-        private int navCount = 0;
+        private final int head1 = 1;
+        private final int head2 = 2;
+        private final int head3 = 3;
+        private final int head4 = 4;
+        private final int head5 = 5;
+        private int dataCount = 0;
         private int state = noneHead;
-        private byte[] gpsItem = new byte[512];
-        private byte[] navItem = new byte[128];
-        private byte[] dataHead = ByteUtil.getBytes("GPT");
-        private byte[] navHead = ByteUtil.getBytes("NAV");
+        private byte[] dataItem = new byte[64];
 
         public void run() {
             while (!interrupted()) {
                 switch (state) {
                     case noneHead:
                         try {
-                            byte head = bytes.poll(2500, TimeUnit.MILLISECONDS);
-                            if (head == 0x24) {
-                                byte head1 = bytes.poll(2500, TimeUnit.MILLISECONDS);
-                                byte head2 = bytes.poll(2500, TimeUnit.MILLISECONDS);
-                                byte head3 = bytes.poll(2500, TimeUnit.MILLISECONDS);
-                                if (head1 == dataHead[0] && head2 == dataHead[1] && head3 == dataHead[2]) {
-                                    state = gpsState;
-                                    gpsCount = 0;
-                                } else if (head1 == navHead[0] && head2 == navHead[1] && head3 == navHead[2]) {
-                                    state = navState;
-                                    navCount = 0;
-                                }
+                            byte head = bytesQueue.poll(2500, TimeUnit.MILLISECONDS);
+                            if (head > 0) {
+                                state = head;
+                                dataCount = 0;
                             }
                         } catch (Exception e) {
-                            Log.d(TAG, "bytes is empty");
+                            Log.d(TAG, "bytesQueue is empty");
                         }
                         break;
-                    case gpsState:
-                        while (!bytes.isEmpty()) {
-                            if (gpsCount < 500) {
+                    case head1:
+                        while (!bytesQueue.isEmpty()) {
+                            if (dataCount < 7) {
                                 try {
-                                    if ((gpsItem[gpsCount++] = bytes.poll(2500, TimeUnit.MILLISECONDS)) == (byte) 0xfd)
-                                        if ((gpsItem[gpsCount++] = bytes.poll(2500, TimeUnit.MILLISECONDS)) == (byte) 0xfd)
-                                            if ((gpsItem[gpsCount++] = bytes.poll(2500, TimeUnit.MILLISECONDS)) == (byte) 0xfd)
-                                                if ((gpsItem[gpsCount++] = bytes.poll(2500, TimeUnit.MILLISECONDS)) == (byte) 0xfd) {
-                                                    if (gpsCount > 160 && gpsCount < 240) {
-                                                        dataParse(Arrays.copyOfRange(gpsItem, 0, gpsCount));
-                                                    }
-                                                    state = noneHead;
-                                                    break;
-                                                }
+                                   dataItem[dataCount++] = bytesQueue.poll(2500, TimeUnit.MILLISECONDS);
                                 } catch (Exception e) {
-                                    Log.i(TAG, "bytes is empty");
+                                    Log.i(TAG, "bytesQueue is empty");
                                 }
                             } else {
+                                dataParse1(Arrays.copyOfRange(dataItem, 0, dataCount));
                                 state = noneHead;
                                 break;
                             }
                         }
                         break;
-                    case navState:
-                        while (!bytes.isEmpty()) {
-                            if (navCount < 120) {
+                    case head2:
+                        while (!bytesQueue.isEmpty()) {
+                            if (dataCount < 15) {
                                 try {
-                                    if ((navItem[navCount++] = bytes.poll(2500, TimeUnit.MILLISECONDS)) == (byte) 0xd)
-                                        if ((navItem[navCount++] = bytes.poll(2500, TimeUnit.MILLISECONDS)) == (byte) 0xa) {
-                                            navParse(navItem, navCount);
-                                            state = noneHead;
-                                            break;
-                                        }
+                                    dataItem[dataCount++] = bytesQueue.poll(2500, TimeUnit.MILLISECONDS);
                                 } catch (Exception e) {
-                                    Log.i(TAG, "bytes is empty");
+                                    Log.i(TAG, "bytesQueue is empty");
                                 }
                             } else {
+                                dataParse2(Arrays.copyOfRange(dataItem, 0, dataCount));
                                 state = noneHead;
                                 break;
                             }
@@ -142,117 +119,12 @@ public class DataParseService extends Service {
             }
         }
 
-        private void dataParse(byte[] dataBuffer) {
-            byte[] gpsByte = new byte[256];
-            byte[] obdByte = new byte[64];
-            int countObd = 4;
-            int countGps = 0;
-            int countBuffer = 0;
-            for (int i = 0; i < 4; i++) {
-                obdByte[i] = (byte) 0xfe;
-            }
-            int length = dataBuffer.length - 2;
-            while (true) {
-                if ((countBuffer < length) && (gpsByte[countGps++] = dataBuffer[countBuffer++]) == (byte) 0xfe) {
-                    if ((gpsByte[countGps++] = dataBuffer[countBuffer++]) == (byte) 0xfe)
-                        if ((gpsByte[countGps++] = dataBuffer[countBuffer++]) == (byte) 0xfe)
-                            if ((gpsByte[countGps++] = dataBuffer[countBuffer++]) == (byte) 0xfe) {
-                                gpsByteParse(gpsByte, countGps - 4);
-                                while (true) {
-                                    if (countObd < 58) {
-                                        if ((obdByte[countObd++] = dataBuffer[countBuffer++]) == (byte) 0xfd)
-                                            if ((obdByte[countObd++] = dataBuffer[countBuffer++]) == (byte) 0xfd)
-                                                if ((obdByte[countObd++] = dataBuffer[countBuffer++]) == (byte) 0xfd)
-                                                    if ((obdByte[countObd++] = dataBuffer[countBuffer++]) == (byte) 0xfd) {
-                                                        if (countObd > 20) {
-                                                            byte[] obdSave = obdByte.clone();
-                                                            fileUtil.write2SDFromInputByte(dir, fileName, obdSave);
-                                                        }
-                                                        break;
-                                                    }
-                                    } else {
-                                        break;
-                                    }
-                                }
-                                break;
-                            }
-                } else if (countBuffer >= length) {
-                    break;
-                }
-            }
+        private void dataParse1(byte[] buffer) {
+
         }
 
-        /**
-         * GPS解析
-         */
-        private void gpsByteParse(byte[] gpsBuffer, int bufferLength) {
-            String info;
-            String gpggaData;
-            String gptraData;
-            try {
-                info = new String(gpsBuffer, 0, bufferLength);
-                info = "$GPT" + info;
-                StringTokenizer gpsTake = new StringTokenizer(info, "$");
-                if (gpsTake.hasMoreTokens()) {
-                    gptraData = gpsTake.nextToken();
-                } else {
-                    return;
-                }
-                if (gpsTake.hasMoreTokens()) {
-                    gpggaData = gpsTake.nextToken();
-                } else {
-                    return;
-                }
-                gpggaAndGptraParse(gpggaData, gptraData);
-                fileUtil.write2SDFromInputString(dir, fileName, info);       //存数据
-            } catch (Exception e) {
-                Log.i(TAG, "parseByteException");
-                e.printStackTrace();
-            }
-        }
+        private void dataParse2(byte[] buffer) {
 
-        private void gpggaAndGptraParse(String gpgga, String gptra) {
-            String strAngle;
-            double[] xy;
-            double lati;
-            double longi;
-            double angle;
-            double time;
-            String[] splitGpgga = gpgga.split(",");
-            String[] splitGptra = gptra.split(",");
-            if (splitGpgga.length > 6 && splitGptra.length > 6) {
-                try {
-                    lati = Double.parseDouble(splitGpgga[2]);
-                    longi = Double.parseDouble(splitGpgga[4]);
-                    time = Float.parseFloat(splitGpgga[1]);
-                } catch (NumberFormatException e) {
-                    return;
-                }
-                time = ((int) time / 100) * 60.0f + time % 100.0f;
-                strAngle = splitGptra[2];
-
-            }
-        }
-
-        /**
-         * 惯导数据
-         */
-        private void navParse(byte[] navItem, int bufferLength) {
-            try {
-                String nav = new String(navItem, 0, bufferLength);
-                String lat;
-                String lon;
-                String heading;
-                nav = "$NAV" + nav;
-                String[] navSplit = nav.split(",");
-                if (navSplit.length > 10) {
-                    lat = navSplit[5];
-                    lon = navSplit[6];
-                    heading = navSplit[9];
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
 
     }
@@ -270,8 +142,8 @@ public class DataParseService extends Service {
         if (translateThread != null) {
             translateThread.interrupt();
         }
-        if (!bytes.isEmpty()) {
-            bytes.clear();
+        if (!bytesQueue.isEmpty()) {
+            bytesQueue.clear();
         }
         if (!queue.isEmpty())
             queue.clear();
