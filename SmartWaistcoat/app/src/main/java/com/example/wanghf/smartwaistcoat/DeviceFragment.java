02685d9f644f6f9b112465a9a35801720b06a26a
wanghf <18810690716@126.com>
2017-05-14 14:25:10 +0800

    滤波

---
+++ SmartWaistcoat/app/src/main/java/com/example/wanghf/smartwaistcoat/DeviceFragment.java
@@ -0,0 +1,11 @@
+package com.example.wanghf.smartwaistcoat;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by wanghf on 2017/5/14.
+ */
+
+public class DeviceFragment extends PreferenceFragment {
+
+}
